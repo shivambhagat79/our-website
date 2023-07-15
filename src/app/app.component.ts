8502939e69547f3ff8e89e14788e8f24a64d8d01
shivambhagat79 <shivambhagat2279+github@gmail.com>
2023-07-15 14:05:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'our-website';
+
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.pageYOffset > 720) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { WordsComponent } from './pages/words/words.component';
+import { SpecialComponent } from './pages/special/special.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    CarouselComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    GalleryComponent,
+    WordsComponent,
+    SpecialComponent
   ],
   imports: [
     BrowserModule,
